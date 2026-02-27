243b950f98f49d24ed568fa645d26edf09a6d239
Ofton-man <junta.jiyuu@gmail.com>
2026-02-27 22:30:03 +0900

    mod名変更

--- src/main/java/net/ofton/oftonmod/OftonMod.java
+++ src/main/java/net/ofton/oftonmod/OftonMod.java
@@ -1,4 +1,4 @@
-package net.ofton.tutorialmod;
+package net.ofton.oftonmod;
 
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.api.distmarker.Dist;
@@ -14,12 +14,12 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(TutorialMod.MOD_ID)
-public class TutorialMod {
-    public static final String MOD_ID = "tutorialmod";
+@Mod(OftonMod.MOD_ID)
+public class OftonMod {
+    public static final String MOD_ID = "ofton_mod";
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    public TutorialMod() {
+    public OftonMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::commonSetup);
