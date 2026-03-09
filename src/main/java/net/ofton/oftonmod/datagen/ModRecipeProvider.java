fbfe60529c241406bdbc2ef666e23e70b43ef6e7
Ofton-man <junta.jiyuu@gmail.com>
2026-03-09 17:23:55 +0900

    レシピとか

--- src/main/java/net/ofton/oftonmod/datagen/ModRecipeProvider.java
+++ src/main/java/net/ofton/oftonmod/datagen/ModRecipeProvider.java
@@ -1,12 +1,14 @@
 package net.ofton.oftonmod.datagen;
 
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.recipes.FinishedRecipe;
-import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.*;
+import net.minecraftforge.common.crafting.conditions.IConditionBuilder;
+import net.ofton.oftonmod.block.ModBlocks;
+import net.ofton.oftonmod.item.ModItems;
 
 import java.util.function.Consumer;
 
-public class ModRecipeProvider extends RecipeProvider {
+public class ModRecipeProvider extends RecipeProvider  implements IConditionBuilder {
 
     public ModRecipeProvider(PackOutput pOutput) {
         super(pOutput);
@@ -14,6 +16,18 @@ public class ModRecipeProvider extends RecipeProvider {
 
     @Override
     protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModBlocks.LEAF_BLOCK.get())
+                .pattern("LLL")
+                .pattern("LLL")
+                .pattern("LLL")
+                .define('L', ModItems.LEAF.get())
+                .unlockedBy(getHasName(ModItems.LEAF.get()),has(ModItems.LEAF.get()))
+                .save(pWriter);
+
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.LEAF.get(),9)
+                .requires(ModBlocks.LEAF_BLOCK.get())
+                .unlockedBy(getHasName(ModBlocks.LEAF_BLOCK.get()), has(ModBlocks.LEAF_BLOCK.get()))
+                .save(pWriter);
 
     }
 }
